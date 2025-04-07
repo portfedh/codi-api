f9df50c8c8db7331fcd1ce9794c20b617d6bdfa1
Portfedh <59422723+portfedh@users.noreply.github.com>
2025-04-07 14:08:21 -0600

    More tests

--- controllers/resultadoOperaciones.js
+++ controllers/resultadoOperaciones.js
@@ -42,7 +42,7 @@ module.exports = {
    *                   0 if successful, negative values for specific errors
    */
   resultadoOperaciones: async (req, res) => {
-    // console.log("Request body: ", req.body);
+    console.log("Request body: ", req.body);
 
     //  Capture request timestamp in Mexico City time
     const requestTimestamp = moment().tz("America/Mexico_City");
