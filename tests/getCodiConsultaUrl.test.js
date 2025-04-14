eeddc57be007ee0064670170e152b595cc3cc700
Portfedh <59422723+portfedh@users.noreply.github.com>
2025-04-13 18:45:46 -0600

    Add tests for getCodiConsultaUrls and remove getCodiStatusURL tests

---
+++ tests/getCodiConsultaUrl.test.js
@@ -0,0 +1,42 @@
+const {
+  getCodiConsultaUrls,
+} = require("../controllers/utils/getCodiConsultaUrl");
+
+describe("getCodiConsultaUrls", () => {
+  afterEach(() => {
+    jest.resetModules(); // Clear the cache
+  });
+
+  it("should return production endpoints when NODE_ENV is 'production'", () => {
+    process.env.NODE_ENV = "production";
+    process.env.SITIO_CODI_CONSULTA_PROD_1 = "https://prod1.example.com";
+    process.env.SITIO_CODI_CONSULTA_PROD_2 = "https://prod2.example.com";
+    const result = getCodiConsultaUrls();
+    expect(result).toEqual({
+      primary: "https://prod1.example.com",
+      secondary: "https://prod2.example.com",
+    });
+  });
+
+  it("should return development endpoints when NODE_ENV is not 'production'", () => {
+    process.env.NODE_ENV = "development";
+    process.env.SITIO_CODI_CONSULTA_DEV_1 = "https://dev1.example.com";
+    process.env.SITIO_CODI_CONSULTA_DEV_2 = "https://dev2.example.com";
+    const result = getCodiConsultaUrls();
+    expect(result).toEqual({
+      primary: "https://dev1.example.com",
+      secondary: "https://dev2.example.com",
+    });
+  });
+
+  it("should return development endpoints when NODE_ENV is undefined", () => {
+    process.env.NODE_ENV = undefined;
+    process.env.SITIO_CODI_CONSULTA_DEV_1 = "https://dev1.example.com";
+    process.env.SITIO_CODI_CONSULTA_DEV_2 = "https://dev2.example.com";
+    const result = getCodiConsultaUrls();
+    expect(result).toEqual({
+      primary: "https://dev1.example.com",
+      secondary: "https://dev2.example.com",
+    });
+  });
+});
