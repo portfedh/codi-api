d90c61d53179938b9b4e2e413c6a749ff4d3c894
Portfedh <59422723+portfedh@users.noreply.github.com>
2025-04-13 18:38:29 -0600

    Refactor getCodiQrUrl tests to getCodiQrUrls and update endpoint assertions

--- tests/getCodiQrUrl.test.js
+++ tests/getCodiQrUrl.test.js
@@ -1,28 +1,40 @@
-const { getCodiQrUrl } = require("../controllers/utils/getCodiQrUrl");
+const { getCodiQrUrls } = require("../controllers/utils/getCodiQrUrl");
 
-describe("getCodiQrUrl", () => {
+describe("getCodiQrUrls", () => {
   afterEach(() => {
     jest.resetModules(); // Clears any cache between tests.
   });
 
-  it("should return the production endpoint when NODE_ENV is 'production'", () => {
+  it("should return production endpoints when NODE_ENV is 'production'", () => {
     process.env.NODE_ENV = "production";
-    process.env.SITIO_CODI_QR_PROD_2 = "https://prod.endpoint.com";
-    const result = getCodiQrUrl();
-    expect(result).toBe("https://prod.endpoint.com");
+    process.env.SITIO_CODI_QR_PROD_1 = "https://prod.primary.com";
+    process.env.SITIO_CODI_QR_PROD_2 = "https://prod.secondary.com";
+    const result = getCodiQrUrls();
+    expect(result).toEqual({
+      primary: "https://prod.primary.com",
+      secondary: "https://prod.secondary.com",
+    });
   });
 
-  it("should return the development endpoint when NODE_ENV is not 'production'", () => {
+  it("should return development endpoints when NODE_ENV is not 'production'", () => {
     process.env.NODE_ENV = "development";
-    process.env.SITIO_CODI_QR_DEV_2 = "https://dev.endpoint.com";
-    const result = getCodiQrUrl();
-    expect(result).toBe("https://dev.endpoint.com");
+    process.env.SITIO_CODI_QR_DEV_1 = "https://dev.primary.com";
+    process.env.SITIO_CODI_QR_DEV_2 = "https://dev.secondary.com";
+    const result = getCodiQrUrls();
+    expect(result).toEqual({
+      primary: "https://dev.primary.com",
+      secondary: "https://dev.secondary.com",
+    });
   });
 
-  it("should return development endpoint when NODE_ENV is undefined", () => {
+  it("should return development endpoints when NODE_ENV is undefined", () => {
     process.env.NODE_ENV = undefined;
-    process.env.SITIO_CODI_QR_DEV_2 = "https://dev.example.com";
-    const result = getCodiQrUrl();
-    expect(result).toBe("https://dev.example.com");
+    process.env.SITIO_CODI_QR_DEV_1 = "https://dev.primary.example.com";
+    process.env.SITIO_CODI_QR_DEV_2 = "https://dev.secondary.example.com";
+    const result = getCodiQrUrls();
+    expect(result).toEqual({
+      primary: "https://dev.primary.example.com",
+      secondary: "https://dev.secondary.example.com",
+    });
   });
 });
